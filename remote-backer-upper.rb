e32760171be85d43578c216016c6077d5077dca0
Dale Karp <me@dale.io>
2012-04-21 16:23:18 -0400

    better error handling

--- remote-backer-upper.rb
+++ remote-backer-upper.rb
@@ -9,33 +9,51 @@ require 'net/scp'
 
 # generate name to be used for backup folder/tar
 bkup_name = Time.now.strftime("%Y-%m-%d_%H-%M_backup")
+
 # open & read config file
-File.open("config.yml") { |file| @config = YAML.load(file) }
+begin
+  File.open("config.yml") { |file| @config = YAML.load(file) }
+rescue Exception => e
+  puts "Cannot find config.yml"
+  exit
+end
 options = { # used in Net::SSH.start
   :password => @config["password"]
 }
+
 # connect to remote host
-Net::SSH.start(@config["host"], @config["user_name"], options) do |ssh|
-  # create backup folder
-  ssh.exec!("mkdir #{bkup_name}")
-  # if any folders to back up exist in config, create 'files' folder and copy
-  if @config["folders"].nil? == false
-    ssh.exec!("mkdir #{bkup_name}/files")
-    @config["folders"].each do |folder|
-      ssh.exec!("cp -a #{folder} #{bkup_name}/files")
+begin
+  Net::SSH.start(@config["host"], @config["user_name"], options) do |ssh|
+    # create backup folder
+    ssh.exec!("mkdir #{bkup_name}")
+
+    # if any folders to back up exist in config, create 'files' folder and copy
+    if @config["folders"].nil? == false
+      ssh.exec!("mkdir #{bkup_name}/files")
+      @config["folders"].each do |folder|
+        ssh.exec!("cp -a #{folder} #{bkup_name}/files")
+      end
     end
-  end
-  # if any databases to back up exist in config, create 'db' folder, backup dbs and copy dumps
-  if @config["databases"].nil? == false
-    ssh.exec!("mkdir #{bkup_name}/db")
-    @config["databases"].each do |db|
-      ssh.exec!("mysqldump -h localhost -u #{db['user_name']} -p#{db['password']} #{db['db_name']} > #{bkup_name}/db/#{db['db_name']}.sql")
+
+    # if any databases to back up exist in config, create 'db' folder, backup dbs and copy dumps
+    if @config["databases"].nil? == false
+      ssh.exec!("mkdir #{bkup_name}/db")
+      @config["databases"].each do |db|
+        ssh.exec!("mysqldump -h localhost -u #{db['user_name']} -p#{db['password']} #{db['db_name']} > #{bkup_name}/db/#{db['db_name']}.sql")
+      end
     end
+
+    # creating a tar of backup folder
+    ssh.exec!("tar -pczf #{bkup_name}.tar #{bkup_name}/")
+
+    # copy folder back to localhost
+    ssh.scp.download!("#{bkup_name}.tar", @config["destination_folder"])
+
+    # clean up files and folders on remote host
+    ssh.exec!("rm -r #{bkup_name} && rm #{bkup_name}.tar") 
   end
-  # creating a tar of backup folder
-  ssh.exec!("tar -pczf #{bkup_name}.tar #{bkup_name}/")
-  # copy folder back to localhost
-  ssh.scp.download!("#{bkup_name}.tar", @config["destination_folder"])
-  # clean up files and folders on remote host
-  ssh.exec!("rm -r #{bkup_name} && rm #{bkup_name}.tar") 
+rescue Exception => e
+  print "There was a problem connecting to the remote host: "
+  puts e
+  exit
 end
